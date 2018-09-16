4d292233bf7a5791f68663aba7877e052a5e6087
manh huy <huy.chatv11@gmail.com>
2018-09-16 20:58:41 +0700

    manhhuy
    tao list san pham moi theo loai
    tao trang chi tiet san pham

--- LTVModule4/DienMayws/Controllers/SanPhamController.cs
+++ LTVModule4/DienMayws/Controllers/SanPhamController.cs
@@ -11,7 +11,8 @@ namespace DienMayws.Controllers
     public class SanPhamController : Controller
     {
         DienMayDBContext db = new DienMayDBContext();
-        
+
+        [ChildActionOnly]//ngăn không cho bên ngoài get trực tiếp vào action
         public PartialViewResult _SanPhamMoiPartial()
         {
             // chỉ định model truy vấn dữ liệu
@@ -23,6 +24,43 @@ namespace DienMayws.Controllers
             return PartialView(items);//pt2
         }
 
+        [ChildActionOnly]//ngăn không cho bên ngoài get trực tiếp vào action
+        public PartialViewResult SanPhamMoi(int idLoai)
+        {
+            // chỉ định model truy vấn dữ liệu
+            Loai loaiItem = db.Loais.Find(idLoai);
+            ViewBag.TenLoai = loaiItem.Ten;
+            List<SanPham> items = db.SanPhams
+                                    .Where(p=>p.LoaiID==idLoai)
+                                    .OrderByDescending(p => p.SanPhamID)
+                                    .Take(8)
+                                    .ToList();
+            // chỉ định partialview "_SanPhamMoiPartial" hiển thị và chuyền dữ liệu qua model object
+            return PartialView("_SanPhamMoiPartial",items);//pt4
+        }
 
+        //Get: SanPham/ChiTiet/417
+        public ActionResult ChiTiet(int? id)
+        {
+            if (id == null || id < 1)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            try
+            {
+                SanPham item = db.SanPhams.Find(id);
+                if (item == null) throw new Exception("Id=" + id + "không tồn tại");
+                //chỉ định view mặc định hiển thị và truyền dữ liệu qua Model object
+                return View(item);//pt3
+            }
+            catch (Exception ex)
+            {
+                object cauBaoLoi = "Lỗi truy cập dữ liệu.</br>Lý do: " + ex.Message;
+                //chỉ định view "ThongBao" hiển thị và truyền dữ liệu qua model object
+                return View("ThongBao",cauBaoLoi);//pt6
+            }
+
+
+        }
     }
 }
