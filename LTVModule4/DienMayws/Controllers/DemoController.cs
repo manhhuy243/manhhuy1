6437dbc3096aae674eee0c09d61d8745b2caf358
manh huy <huy.chatv11@gmail.com>
2018-09-16 17:45:20 +0700

    manhhuy
    thiet ke layout trang chu web

---
+++ LTVModule4/DienMayws/Controllers/DemoController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DienMayws.Models;
+
+namespace DienMayws.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo/TestModel
+        public ViewResult TestModel()
+        {
+            DienMayDBContext db = new DienMayDBContext();
+            List<Loai> items = db.Loais.ToList();
+            return View();
+        }
+        //Get: Demo/TestDienMayLayout
+        public ViewResult TestDienMayLayout()
+        {
+            return View();
+        }
+    }
+}
---
+++ LTVModule4/DienMayws/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DienMayws.Startup))]
+namespace DienMayws
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
